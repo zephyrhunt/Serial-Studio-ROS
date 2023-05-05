0e84d94d77c84033a0dd78481711bbbb99957e66
nichijou-hp-omen <1534150771@qq.com>
2023-05-06 00:18:42 +0800

    将工程打包为ROS PACK

---
+++ src/ROS/QNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nichijou on 23-5-5.
+//
+
+#include "QNode.h"
---
+++ src/ROS/QNode.h
@@ -0,0 +1,12 @@
+//
+// Created by nichijou on 23-5-5.
+//
+
+#ifndef SERIAL_STUDIO_QNODE_H
+#define SERIAL_STUDIO_QNODE_H
+
+class QNode
+{
+};
+
+#endif // SERIAL_STUDIO_QNODE_H
